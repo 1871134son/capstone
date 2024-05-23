073e906fff731fed4074361465b6e87c5d3560ad
son <adsfg4295@gmail.com>
2024-05-24 07:57:33 +0900

    son

---
+++ logincapstone/src/routes/Write.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import { boardSave, getUserName, getBrdno, updateBrdno} from '../firebase/firebase.js';
+import styles from './Write.module.css';
+
+function Write() {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [brddate, setBrddate] = useState('');
+
+    //brdno +1
+    const [brdno, setBrdno] = useState(0); // 게시글 번호를 위한 상태 변수
+
+    
+    useEffect(() => {
+        setBrddate(Date.now()); // Set current date
+
+        // Firestore에서 현재 게시글 번호를 가져옴
+        const fetchBrdno = async () => {
+            const currentBrdno = await getBrdno();
+            setBrdno(currentBrdno);
+        };
+        fetchBrdno();
+    }, []);
+
+
+    const navigate = useNavigate();
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const brdwriter = await getUserName(); // Get the writer's information
+
+        const newBrdno = brdno + 1; // 새로운 게시글 번호 계산
+        await boardSave(newBrdno, title, content, brddate, brdwriter);
+        await updateBrdno(newBrdno); // Firestore에 새로운 게시글 번호 저장
+
+        navigate('/postlist');
+    };
+
+
+    return (
+        <main role="main" className={styles.container}>
+            <h2 className={styles.writeTitle}>글쓰기</h2>
+            <Form onSubmit={handleSubmit}>
+                <div className={styles.pt1}>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>제목</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            placeholder="제목을 입력하세요"
+                            required
+                            className={styles.input}
+                        />
+                    </Form.Group>
+                </div>
+                <div className={styles.pt1}>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>내용</Form.Label>
+                        <Form.Control
+                            as="textarea"
+                            value={content}
+                            onChange={(e) => setContent(e.target.value)}
+                            placeholder="내용을 입력하세요"
+                            required
+                            className={styles.textarea}
+                        />
+                    </Form.Group>
+                </div>
+                <div className={`${styles.pt1} ${styles.textRight}`}>
+                    <Button type="submit" variant="success" className={styles.submitButton}>
+                        작성완료
+                    </Button>
+                    <Link to="/postlist">
+                        <Button variant="secondary" type="button" className={styles.cancelButton}>
+                            취소
+                        </Button>
+                    </Link>
+                </div>
+            </Form>
+        </main>
+    );
+}
+
+export default Write;
---
+++ logincapstone/src/routes/post/PostList.js
@@ -0,0 +1,125 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import CommonTable from '../../routes/component/table/CommonTable';
+import CommonTableColumn from '../../routes/component/table/CommonTableColumn';
+import CommonTableRow from '../../routes/component/table/CommonTableRow';
+import dateFormat from 'dateformat';
+import { fetchPostsFromFirebase } from '../../firebase/firebase.js';
+import './PostList.css';
+
+const PostMain = () => {
+  const [dataList, setDataList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const posts = await fetchPostsFromFirebase();
+        setDataList(posts);
+        
+        
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+
+  const filteredPosts = dataList.filter(item =>
+    item.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const currentPosts = filteredPosts.slice(indexOfFirstPost, indexOfLastPost);
+
+  const paginate = (pageNumber) => {
+    if (pageNumber < 1 || pageNumber > Math.ceil(filteredPosts.length / postsPerPage)) {
+      return;
+    }
+    setCurrentPage(pageNumber);
+  };
+
+  const goToPrevPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  const goToNextPage = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, Math.ceil(filteredPosts.length / postsPerPage)));
+  };
+
+  const goToFirstPage = () => {
+    setCurrentPage(1);
+  };
+
+  const goToLastPage = () => {
+    setCurrentPage(Math.ceil(filteredPosts.length / postsPerPage));
+  };
+
+  const handleItemsPerPageChange = e => {
+    setPostsPerPage(parseInt(e.target.value));
+    setCurrentPage(1);
+  };
+
+  return (
+    <>
+      <div className="board_wrap">
+        <div className="board_title">
+          <strong>게시판</strong>
+        </div>
+        <div className="items-per-page">
+          <span>Show: </span>
+          <select value={postsPerPage} onChange={handleItemsPerPageChange}>
+            <option value={5}>5</option>
+            <option value={10}>10</option>
+            <option value={15}>15</option>
+            <option value={20}>20</option>
+          </select>
+          <span> items per page</span>
+        </div>
+        <div className="board_list_wrap">
+          <div className="board_list">
+            <CommonTable className="top" headersName={['글번호', '제목', '등록일', '작성자']}>
+              {currentPosts.map((item, index) => (
+                <CommonTableRow key={index}>
+                  <CommonTableColumn className="num"><div className="post-number">{item.brdno}</div></CommonTableColumn>
+                  <CommonTableColumn>
+                    <Link to={`/postView/${item.id}`}className="titlehover">{item.title}</Link>
+                  </CommonTableColumn>
+                  <CommonTableColumn>{dateFormat(item.brddate, "yyyy-mm-dd")}</CommonTableColumn>
+
+                  <CommonTableColumn>{item.brdwriter}</CommonTableColumn>
+                </CommonTableRow>
+              ))}
+            </CommonTable>
+          </div>
+          <div className="board_page">
+            <a href="#" className="bt first" onClick={goToFirstPage}>{"<<"}</a>
+            <a href="#" className="bt prev" onClick={goToPrevPage}>{"<"}</a>
+            {Array.from({ length: Math.ceil(filteredPosts.length / postsPerPage) }, (_, i) => i + 1).map(number => (
+              <Link key={number} to="#" className={`num ${currentPage === number ? 'on' : ''}`} onClick={() => paginate(number)}>
+                {number}
+              </Link>
+            ))}
+            <a href="#" className="bt next" onClick={goToNextPage}>{">"}</a>
+            <a href="#" className="bt last" onClick={goToLastPage}>{">>"}</a>
+          </div>
+          {/* "글쓰기" 버튼을 감싸는 Link 컴포넌트, custom-write-button 클래스를 사용하여 스타일 적용 */}
+          <Link to="/write">
+            <button className="custom-write-button">글쓰기</button>
+          </Link>
+
+          <div className="bt_wrap">
+            <a href="#" className="on">목록</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PostMain;
