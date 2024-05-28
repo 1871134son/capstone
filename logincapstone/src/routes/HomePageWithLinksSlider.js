7bdead2988b222ee66be3d0bab5b0dcc519efe98
sonjiho <adsfg4295@gmail.com>
2024-05-28 10:17:22 +0900

    son

---
+++ logincapstone/src/routes/HomePageWithLinksSlider.js
@@ -0,0 +1,68 @@
+// HomePageWithLinksSlider.js
+import React, { useState, useEffect } from 'react';
+import Slider from 'react-slick';
+import { DisplayImage } from "../firebase/firebase";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './HomePageWithLinksSlider.css';
+
+function HomePageWithLinksSlider() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000
+    };
+
+    return (
+        <div className={`color ${isScrolled ? 'scrolled' : ''}`}>
+            <hr></hr>
+            <div className="slider-container">
+                <Slider {...settings}>
+                    <div>
+                        <a href="/linkA" className="slider-link">
+                            <DisplayImage folderName="promotions" fileName="promotions1.jpg" className="slider-image" />
+                        </a>
+                    </div>
+                    <div>
+                        <a href="/linkB" className="slider-link">
+                            <DisplayImage folderName="promotions" fileName="promotions2.jpg" className="slider-image" />
+                        </a>
+                    </div>
+                    <div>
+                        <a href="/linkC" className="slider-link">
+                            <DisplayImage folderName="promotions" fileName="promotions.jpg" className="slider-image" />
+                        </a>
+                    </div>
+                    <div>
+                        <a href="/linkD" className="slider-link">
+                            <DisplayImage folderName="promotions" fileName="promotions4.jpg" className="slider-image" />
+                        </a>
+                    </div>
+                </Slider>
+            </div>
+        </div>
+    );
+}
+
+export default HomePageWithLinksSlider;
